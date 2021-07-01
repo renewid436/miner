48a831d0d04431115f09d454c80624790e2a3e3a
renewid436 <86760119+renewid436@users.noreply.github.com>
2021-07-01 11:26:54 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9Hi83wLMyY25XYf6yL3dv9kmt3uVjJrxk.Ghost1 -p x --cpu 4
